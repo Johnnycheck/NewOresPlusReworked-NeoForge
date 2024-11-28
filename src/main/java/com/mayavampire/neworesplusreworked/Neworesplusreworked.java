aeafb27cbd5795a98dc8f3af67b16135854b4b12
Johnnycheck <johnnycheck1234@gmail.com>
2024-11-28 03:34:22 -0600

    Changed something's around with the block id names and add the end Sapphire Ore spawn.

--- src/main/java/com/mayavampire/neworesplusreworked/Neworesplusreworked.java
+++ src/main/java/com/mayavampire/neworesplusreworked/Neworesplusreworked.java
@@ -1,11 +1,11 @@
 package com.mayavampire.neworesplusreworked;
 
+
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -29,9 +29,6 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 
-import java.rmi.registry.Registry;
-
-import static net.minecraft.world.item.ToolMaterial.DIAMOND;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Neworesplusreworked.MODID)
@@ -47,34 +44,30 @@ public class Neworesplusreworked {
     // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "neworesplusreworked" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create ( Registries.CREATIVE_MODE_TAB , MODID );
 
-    // Creates a new Block with the id "neworesplusreworked:example_block", combining the namespace and path
-    public static final DeferredBlock<Block> SAPPHIRE_BLOCK = BLOCKS.registerSimpleBlock ( "sapphire_block", BlockBehaviour.Properties.of ().sound ( SoundType.AMETHYST ).strength ( 3.0f, 10f).requiresCorrectToolForDrops () );
-    public static final DeferredItem<BlockItem> SAPPHIRE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem ( "sapphire_block_item", SAPPHIRE_BLOCK );
-    public static final DeferredItem<Item> SAPPHIRE_ITEM = ITEMS.registerSimpleItem ( "sapphire_item");
-    public static final DeferredBlock<Block> SAPPHIRE_ORE = BLOCKS.registerSimpleBlock ( "sapphire_ore", BlockBehaviour.Properties.of ().sound ( SoundType.AMETHYST ).strength ( 3.0f, 10f).requiresCorrectToolForDrops () );
-    public static final DeferredItem<BlockItem> SAPPHIRE_ORE_ITEM = ITEMS.registerSimpleBlockItem ( "sapphire_ore_item", SAPPHIRE_ORE );
-    public static final DeferredItem<Item> SAPPHIRE_AXE = ITEMS.registerSimpleItem ( "sapphire_axe" , new Item.Properties ().durability ( 19902 ).rarity ( Rarity.EPIC ));
-    public static final DeferredItem<Item> RAW_SAPPHIRE = ITEMS.registerSimpleItem ( "raw_sapphire" );
+   // Creates a new Block with the id "neworesplusreworked:example_block", combining the namespace and path
+    public static final DeferredBlock<Block> SAPPHIRE_BLOCK = BLOCKS.registerSimpleBlock ( "sapphire_block", BlockBehaviour.Properties.ofFullCopy (Blocks.IRON_BLOCK).sound ( SoundType.AMETHYST ).strength ( 3.0f, 10f).requiresCorrectToolForDrops () );
+    public static final DeferredItem<BlockItem> SAPPHIRE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem ( "sapphire_block", SAPPHIRE_BLOCK );
+    public static final DeferredItem<Item> SAPPHIRE = ITEMS.registerSimpleItem ( "sapphire");
+    public static final DeferredBlock<Block> SAPPHIRE_ORE = BLOCKS.registerSimpleBlock ( "sapphire_ore" , BlockBehaviour.Properties.ofFullCopy ( Blocks.DIAMOND_ORE ).requiresCorrectToolForDrops ().sound ( SoundType.AMETHYST ).destroyTime ( 2f ));
+    public static final DeferredItem<Item> RAW_SAPPHIRE = ITEMS.registerSimpleItem ( "raw_sapphire", new Item.Properties ());
+    public static final DeferredItem<BlockItem> SAPPHIRE_ORE_ITEM = ITEMS.registerSimpleBlockItem ("sapphire_ore", SAPPHIRE_ORE  );
     public static final DeferredBlock<Block> DEEPSLATE_SAPPHIRE_ORE = BLOCKS.registerSimpleBlock ( "deepslate_sapphire_ore", BlockBehaviour.Properties.of ().sound ( SoundType.AMETHYST ).strength ( 5.3f, 10f ).requiresCorrectToolForDrops () );
-    public static final DeferredItem<BlockItem> DEEPSLATE_SAPPHIRE_ORE_ITEM = ITEMS.registerSimpleBlockItem ( "deepslate_sapphire_ore_item", DEEPSLATE_SAPPHIRE_ORE );
+    public static final DeferredItem<BlockItem> DEEPSLATE_SAPPHIRE_ORE_ITEM = ITEMS.registerSimpleBlockItem ( "deepslate_sapphire_ore", DEEPSLATE_SAPPHIRE_ORE );
     public static final DeferredBlock<Block> NETHER_SAPPHIRE_ORE = BLOCKS.registerSimpleBlock ( "nether_sapphire_ore", BlockBehaviour.Properties.of ().sound ( SoundType.NETHERRACK ).strength ( 1f, 0.4f  ).requiresCorrectToolForDrops () );
-    public static final DeferredItem<BlockItem> NETHER_SAPPHIRE_ORE_ITEM = ITEMS.registerSimpleBlockItem ( "nether_sapphire_ore_item", NETHER_SAPPHIRE_ORE );
-    public static final DeferredItem<Item> SAPPHIRE_SWORD;
-
-    static {
-        SAPPHIRE_SWORD = ITEMS.registerSimpleItem ( "sapphire_sword" );
-    }
-
+    public static final DeferredItem<BlockItem> NETHER_SAPPHIRE_ORE_ITEM = ITEMS.registerSimpleBlockItem ( "nether_sapphire_ore", NETHER_SAPPHIRE_ORE );
+    public static final DeferredBlock<Block> END_SAPPHIRE_ORE = BLOCKS.registerSimpleBlock ( "end_sapphire_ore", BlockBehaviour.Properties.ofFullCopy ( Blocks.IRON_ORE ).sound ( SoundType.AMETHYST ) .strength (  3f, 9f ).requiresCorrectToolForDrops () );
+    public static final DeferredItem<BlockItem> END_SAPPHIRE_ORE_ITEM = ITEMS.registerSimpleBlockItem ( "end_sapphire_ore", END_SAPPHIRE_ORE );
     // Creates a creative tab with the id "neworesplusreworked:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register ( "example_tab" , () -> CreativeModeTab.builder ().title ( Component.translatable ( "itemGroup.neworesplusreworked" ) ).withTabsBefore ( CreativeModeTabs.COMBAT ).icon ( () -> SAPPHIRE_ITEM.get ().getDefaultInstance () ).displayItems ( (parameters , output) -> {
-        output.accept ( SAPPHIRE_ITEM.get () );
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register ( "example_tab" , () -> CreativeModeTab.builder ().title ( Component.translatable ( "itemGroup.neworesplusreworked" ) ).withTabsBefore ( CreativeModeTabs.COMBAT ).icon ( () -> SAPPHIRE.get ().getDefaultInstance () ).displayItems ( (parameters , output) -> {
+        output.accept ( SAPPHIRE.get () );
         output.accept ( SAPPHIRE_BLOCK_ITEM );
-        output.accept ( SAPPHIRE_ORE_ITEM );
-        output.accept (SAPPHIRE_AXE );
         output.accept ( RAW_SAPPHIRE );
         output.accept ( DEEPSLATE_SAPPHIRE_ORE_ITEM );
         output.accept (NETHER_SAPPHIRE_ORE_ITEM);
+        output.accept (SAPPHIRE_ORE_ITEM );
+        output.accept ( END_SAPPHIRE_ORE_ITEM );
     } ).build () );
+
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
